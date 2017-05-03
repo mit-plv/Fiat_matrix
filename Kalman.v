91e72b9273dc4266ba660ec51fd521b6bf986514
Yinzhan Xu <xyzhan@mit.edu>
2017-05-02 22:01:31 -0400

    add Cholesky Decomposation

--- Kalman.v
+++ Kalman.v
@@ -34,6 +34,64 @@ Axiom Id : forall {n}, SDM n.
 Arguments Mtimes : simpl never.
 (* Arguments DenseMatrix : simpl never. *)
 
+Theorem refine_change_type A B:
+  forall (a : Comp A) (f: A -> Comp B) (cast: A -> B) (cast_back: B -> A),
+    (forall x, refine (f x) (f (cast_back (cast x)))) -> 
+    refine (x <- a; f x)
+           (x <- (y <- a; ret (cast y));
+              f (cast_back x)).
+Proof.
+  intros.
+  rewrite <- refine_bind_bind'.
+  unfold refine.
+  Transparent computes_to. 
+  unfold computes_to.
+  Transparent Bind.
+  unfold Bind.
+  intros. intros. 
+  inversion H0; subst.
+  exists x.
+  split.
+  - apply H1.
+  - inversion H1; subst.
+    unfold Ensembles.In in H3.
+    inversion H3; subst; clear H3.
+    inversion H4; subst; clear H4. 
+    rewrite H.
+    unfold Ensembles.In. inversion H3; subst.
+    assumption.
+Qed.
+
+Section Test. 
+Definition P1 := x <- { x | x > 0}; y <- ret (x*2); ret (x + y).
+Theorem Ref : {P' : Comp nat |   refine P1 P'}.
+Proof.
+  unfold P1.
+  eexists.
+  refine pick val 3; auto.
+  simplify with monad laws.
+  reflexivity.
+Defined.
+Print exist.
+Print proj1_sig.
+Print proj2_sig.
+
+Eval cbv [proj1_sig Ref] in  proj1_sig Ref. 
+Compute proj1_sig Ref.
+
+(*  unfold refine .
+  
+  Transparent computes_to. 
+  unfold computes_to.
+  Transparent Bind.
+  unfold Bind.
+  
+  About Ensembles.Ensemble. 
+  Unset Printing Notations.
+  
+  ** work to do: add decompose, and incorporate into the following opt code. 
+  *)
+End Test. 
 Section KalmanFilter.
   Variable n : nat.
 
@@ -73,7 +131,7 @@ Section KalmanFilter.
     | [ H := ?x : methodType _ _ _ |- _ ] => is_evar x; progress unfold H
     (* | [ H := ?x : constructorType _ _ |- _ ] => is_evar x; progress unfold H *)
     end.
-
+   
   Ltac cleanup :=
     repeat match goal with
            | [ H: _ /\ _ |- _ ] => destruct H
@@ -107,8 +165,18 @@ Section KalmanFilter.
     Axiom matrix_eq_commutes :
       forall (m n: nat) ME M1 M2 (m1: @Mt ME M1 m n) (m2: @Mt ME M2 m n),
         m1 @= m2 -> m2 @= m1.
-
-    Hint Resolve sparsify_correct densify_correct matrix_eq_commutes: matrices.
+    Axiom solveR: forall {n}, SDM n -> SDM n -> SDM n.
+    Axiom solveR_correct: forall n: nat, forall M1 M2:SDM n,
+      M1 @* (inversion M2) = solveR M2 M1.
+    Axiom multi_assoc: forall n: nat, forall M1 M2 M3 : SDM n,
+          (M1 @* M2) @* M3 = M1 @* (M2 @* M3).
+    Axiom Cholesky_DC: forall {n}, SDM n -> SDM n.
+    Axiom solveR_lower: forall {n}, SDM n -> SDM n -> SDM n.
+    Axiom solveR_upper: forall {n}, SDM n -> SDM n -> SDM n.
+    Axiom Cholesky_DC_correct: forall n:nat, forall M1 M2 : SDM n, 
+        solveR M1 M2 = solveR_upper (transpose (Cholesky_DC M1)) (solveR_lower (Cholesky_DC M1) M2). 
+    
+    Hint Resolve sparsify_correct densify_correct matrix_eq_commutes solveR_correct multi_assoc: matrices.
 
     hone representation using use_a_sparse_P;
       unfold use_a_sparse_P in *; cleanup; try reveal_body_evar.
@@ -126,8 +194,10 @@ Section KalmanFilter.
       rewrite !H0, !H2.
 
       refine blocked ret.
+      (*rewrite blocked_ret_is_ret.
+      simplify with monad laws.*)
       refine blocked ret.
-
+      
       refine pick val r_n.
       - simplify with monad laws; finish honing.
       - eauto with matrices.
@@ -139,6 +209,11 @@ Section KalmanFilter.
       assert (r_o.(P) = densify (r_n.(SP))) by (apply (magic tt)).
       rewrite !H0, !H2.
 
+      refine blocked ret.
+      refine blocked ret.
+      rewrite multi_assoc. 
+      rewrite solveR_correct. 
+      rewrite Cholesky_DC_correct. 
       repeat refine blocked ret.
 
       refine pick val {| Sx := bvar2; SP := sparsify bvar3 |}.
@@ -161,7 +236,7 @@ Section KalmanFilter.
   (*     refine a' a -> *)
   (*     refine (Bind a f) c -> *)
   (*     refine (Bind a' f) c. *)
-  (* Proof. *)
+a  (* Proof. *)
   (*   intros. *)
   (*   etransitivity. *)
   (*   apply refine_under_bind_both; eauto. *)

0f24dd479a8e46976734a7a055fbc684e50953f6
Yinzhan Xu <xyzhan@mit.edu>
2017-12-12 12:52:07 -0500

    cleaned up the code a bit

--- Kalman.v
+++ Kalman.v
@@ -10,87 +10,10 @@ Require Import
         SparseMatrix
         DenseMatrix
         FiatHelpers
-        MyHelpers.
+        MyHelpers
+        MatrixLemmas.
 
 
-
-Variable E: MatrixElem.
-Notation SDM n := (Mt (ME := E) (Matrix := DenseMatrix) n n).
-Notation SSM n := (Mt (ME := E) (Matrix := SparseMatrix) n n).
-Notation Vt n := (Mt (ME := E) (Matrix := DenseMatrix) n 1).
-Opaque DenseMatrix.
-Opaque SparseMatrix. 
-
-
-Definition transpose {n} (A : SDM n) :=
-  @Mfill E DenseMatrix n n (fun i j => Mget A j i).
-Definition MVtimes {n} (M: SDM n) (v: Vt n) := M @* v.
-Axiom inversion : forall {n}, SDM n -> SDM n.
-
-Global Add Parametric Morphism n: (MVtimes ) with
-      signature (Meq (m:=n)(n:=n)) ==> (Meq (m:=n)(n:=1)) ==> (Meq (m:=n)(n:=1)) as MVtimes_mor.
-Admitted.
-
-Global Add Parametric Morphism n: (transpose ) with
-      signature (Meq (m:=n)(n:=n)) ==> (Meq (m:=n)(n:=n)) as transpose_mor.
-Admitted.
-
-
-Global Add Parametric Morphism n: (inversion ) with
-      signature (Meq (m:=n)(n:=n)) ==> (Meq (m:=n)(n:=n)) as inversion_mor.
-Admitted.
-
-Infix "&*" := MVtimes (at level 40, left associativity) : matrix_scope.
-Definition Vplus {n} (v: Vt n) (u: Vt n):= v @+ u.
-Infix "&+" := Vplus (at level 50, left associativity) : matrix_scope.
-
-Global Add Parametric Morphism n: (Vplus ) with
-      signature (Meq (m:=n)(n:=1)) ==> (Meq (m:=n)(n:=1)) ==> (Meq (m:=n)(n:=1)) as Vplus_mor.
-Admitted.
-
-Definition Vminus {n} (v: Vt n) (u: Vt n):= v @- u.
-Infix "&-" := Vminus (at level 50, left associativity) : matrix_scope.
-
-Global Add Parametric Morphism n: (Vminus ) with
-      signature (Meq (m:=n)(n:=1)) ==> (Meq (m:=n)(n:=1)) ==> (Meq (m:=n)(n:=1)) as Vminus_mor.
-Admitted.
-
-Definition Id {n} := I n E DenseMatrix.
-
-Arguments Mtimes : simpl never.
-(* Arguments DenseMatrix : simpl never. *)
-
-
-Section Test. 
-Definition P1 := x <- { x | x > 0}; y <- ret (x*2); ret (x + y).
-Theorem Ref : {P' : Comp nat |   refine P1 P'}.
-Proof.
-  unfold P1.
-  eexists.
-  refine pick val 3; auto.
-  simplify with monad laws.
-  reflexivity.
-Defined.
-Print exist.
-Print proj1_sig.
-Print proj2_sig.
-
-Eval cbv [proj1_sig Ref] in  proj1_sig Ref. 
-Compute proj1_sig Ref.
-
-(*  unfold refine .
-  
-  Transparent computes_to. 
-  unfold computes_to.
-  Transparent Bind.
-  unfold Bind.
-  
-  About Ensembles.Ensemble. 
-  Unset Printing Notations.
-  
-  ** work to do: add decompose, and incorporate into the following opt code. 
-  *)
-End Test. 
 Section KalmanFilter.
   Variable n : nat.
 
@@ -158,100 +81,6 @@ Section KalmanFilter.
 
     Open Scope string_scope.
 
-    Definition sparsify {n} (A: SDM n) := @Mfill E SparseMatrix n n (fun i j => Mget A i j). 
-    Lemma sparsify_correct: forall n: nat, forall M : SDM n, M @= sparsify M.
-    Proof.
-      intros.
-      unfold sparsify.
-      unfold "@=".
-      intros. 
-      rewrite Mfill_correct; try assumption.
-      reflexivity.
-    Qed.
-
-    (* can be optimized *) 
-    Definition densify {n} (A: SSM n) := @Mfill E DenseMatrix n n (fun i j => Mget A i j). 
-    Lemma densify_correct: forall n: nat, forall M : SSM n, M @= densify M.
-    Proof.
-      intros.
-      unfold densify.
-      unfold "@=".
-      intros.
-      rewrite Mfill_correct; try assumption.
-      reflexivity.
-    Qed.
-    Lemma densify_correct_rev: forall n: nat, forall M : SSM n,  densify M @= M.
-    Proof.
-      intros.
-      unfold densify.
-      unfold "@=".
-      intros.
-      rewrite Mfill_correct; try assumption.
-      reflexivity.
-    Qed.
-    
-    Lemma matrix_eq_commutes :
-      forall (m n: nat) ME M1 M2 (m1: @Mt ME M1 m n) (m2: @Mt ME M2 m n),
-        m1 @= m2 -> m2 @= m1.
-    Proof.
-      intros.
-      unfold "@=" in *.
-      intros.
-      rewrite H; auto.
-    Qed.
-    
-    Axiom solveR: forall {n}, SDM n -> SDM n -> SDM n.
-    Axiom solveR_correct: forall n: nat, forall M1 M2:SDM n,
-      M1 @* (inversion M2) = solveR M2 M1.
-    Lemma multi_assoc: forall n: nat, forall M1 M2 M3 : SDM n,
-          (M1 @* M2) @* M3 @= M1 @* (M2 @* M3).
-    Proof.
-      intros.
-      rewrite mult_assoc.
-      reflexivity.
-    Qed.
-    
-    Axiom Cholesky_DC: forall {n}, SDM n -> SDM n.
-    Axiom solveR_lower: forall {n}, SDM n -> SDM n -> SDM n.
-    Axiom solveR_upper: forall {n}, SDM n -> SDM n -> SDM n.
-    Lemma ABv_assoc: forall n:nat, forall A B: SDM n, forall v : Vt n,
-            MVtimes (A @* B) v @=  MVtimes A (MVtimes B v).
-    Proof.
-      intros.
-      unfold "&*".
-      rewrite mult_assoc.
-      reflexivity.
-    Qed.
-    
-    Axiom Cholesky_DC_correct: forall n:nat, forall M1 M2 : SDM n, 
-          solveR M1 M2 = solveR_upper (transpose (Cholesky_DC M1)) (solveR_lower (Cholesky_DC M1) M2).
-
-    Axiom Densify_correct: forall n:nat, forall M : SDM n, forall S : SSM n,
-            M @= S -> M @= densify S.
-    
-    
-    Lemma Densify_correct_rev: forall n:nat, forall M : SDM n, forall S : SSM n,
-            M @= densify S -> M @= S.
-    Proof.
-      intros.
-      unfold densify in H.
-      unfold "@=" in *.
-      intros.
-      rewrite H; auto. 
-      rewrite Mfill_correct; auto.
-    Qed.
-    
-    Axiom dense_sparse_mul: forall {n}, SDM n -> SSM n -> SDM n.
-    Axiom dense_sparse_mul_correct: forall {n}, forall A: SDM n, forall B: SSM n, 
-            dense_sparse_mul A B = A @* densify B. 
-    Axiom sparse_dense_mul: forall {n}, SSM n -> SDM n -> SDM n.
-    Axiom sparse_dense_mul_correct: forall {n}, forall A: SSM n, forall B: SDM n, 
-            sparse_dense_mul A B = densify A @*  B.
-    Axiom dense_sparse_mul_to_sparse: forall {n}, SDM n -> SSM n -> SSM n.
-    Axiom dense_sparse_mul_to_sparse_correct: forall {n}, forall A: SDM n, forall B: SSM n, 
-            sparsify (dense_sparse_mul A B) = dense_sparse_mul_to_sparse A B. 
-    Hint Resolve sparsify_correct densify_correct densify_correct_rev matrix_eq_commutes solveR_correct multi_assoc Densify_correct Densify_correct_rev dense_sparse_mul_correct sparse_dense_mul_correct dense_sparse_mul_to_sparse_correct eq_Mt_refl: matrices.
-    
     hone representation using use_a_sparse_P;
       unfold use_a_sparse_P in *; cleanup; try reveal_body_evar.
 
@@ -274,6 +103,7 @@ Section KalmanFilter.
                  subst g;
                  subst f
                end.
+    
     Ltac guess_pick_val r_o r_n:=
         let x := fresh "x" in
         let SP := fresh "SP" in
@@ -425,6 +255,20 @@ Section KalmanFilter.
       }
 
       {
+        etransitivity. 
+          substitute_all.
+          clear_a_Meq_pick r_o r_n.
+          erewrite pick_change_condition.
+          Focus 2. let X := fresh "X" in let H := fresh "H" in intros X H.
+          clearit r_o r_n.
+          apply H]; refine pick val _; [ | apply eq_Mt_refl]; simplify with monad laws.
+
+          
+          guess_pick_val r_o r_n;
+          try simplify with monad laws;
+          higher_order_reflexivity;
+          simpl | ].
+        
         Optimize_single_method r_o r_n.
       }
       
---
+++ MatrixLemmas.v
@@ -0,0 +1,153 @@
+Require Import
+        Coq.Lists.List
+        Coq.Strings.String
+        Coq.Setoids.Setoid
+        Coq.Arith.PeanoNat
+        Coq.omega.Omega
+        Coq.setoid_ring.Ring
+        Coq.setoid_ring.Ring_theory
+        Matrix
+        SparseMatrix
+        DenseMatrix
+        MyHelpers.
+
+
+Variable E: MatrixElem.
+Notation SDM n := (Mt (ME := E) (Matrix := DenseMatrix) n n).
+Notation SSM n := (Mt (ME := E) (Matrix := SparseMatrix) n n).
+Notation Vt n := (Mt (ME := E) (Matrix := DenseMatrix) n 1).
+Global Opaque DenseMatrix.
+Global Opaque SparseMatrix. 
+
+
+Definition transpose {n} (A : SDM n) :=
+  @Mfill E DenseMatrix n n (fun i j => Mget A j i).
+Definition MVtimes {n} (M: SDM n) (v: Vt n) := M @* v.
+Axiom inversion : forall {n}, SDM n -> SDM n.
+
+Global Add Parametric Morphism n: (MVtimes ) with
+      signature (Meq (m:=n)(n:=n)) ==> (Meq (m:=n)(n:=1)) ==> (Meq (m:=n)(n:=1)) as MVtimes_mor.
+Admitted.
+
+Global Add Parametric Morphism n: (transpose ) with
+      signature (Meq (m:=n)(n:=n)) ==> (Meq (m:=n)(n:=n)) as transpose_mor.
+Admitted.
+
+
+Global Add Parametric Morphism n: (inversion ) with
+      signature (Meq (m:=n)(n:=n)) ==> (Meq (m:=n)(n:=n)) as inversion_mor.
+Admitted.
+
+Infix "&*" := MVtimes (at level 40, left associativity) : matrix_scope.
+Definition Vplus {n} (v: Vt n) (u: Vt n):= v @+ u.
+Infix "&+" := Vplus (at level 50, left associativity) : matrix_scope.
+
+Global Add Parametric Morphism n: (Vplus ) with
+      signature (Meq (m:=n)(n:=1)) ==> (Meq (m:=n)(n:=1)) ==> (Meq (m:=n)(n:=1)) as Vplus_mor.
+Admitted.
+
+Definition Vminus {n} (v: Vt n) (u: Vt n):= v @- u.
+Infix "&-" := Vminus (at level 50, left associativity) : matrix_scope.
+
+Global Add Parametric Morphism n: (Vminus ) with
+      signature (Meq (m:=n)(n:=1)) ==> (Meq (m:=n)(n:=1)) ==> (Meq (m:=n)(n:=1)) as Vminus_mor.
+Admitted.
+
+Definition Id {n} := I n E DenseMatrix.
+
+Arguments Mtimes : simpl never.
+
+
+Definition sparsify {n} (A: SDM n) := @Mfill E SparseMatrix n n (fun i j => Mget A i j). 
+Lemma sparsify_correct: forall n: nat, forall M : SDM n, M @= sparsify M.
+Proof.
+  intros.
+  unfold sparsify.
+  unfold "@=".
+  intros. 
+  rewrite Mfill_correct; try assumption.
+  reflexivity.
+Qed.
+
+(* can be optimized *) 
+Definition densify {n} (A: SSM n) := @Mfill E DenseMatrix n n (fun i j => Mget A i j). 
+Lemma densify_correct: forall n: nat, forall M : SSM n, M @= densify M.
+Proof.
+  intros.
+  unfold densify.
+  unfold "@=".
+  intros.
+  rewrite Mfill_correct; try assumption.
+  reflexivity.
+Qed.
+Lemma densify_correct_rev: forall n: nat, forall M : SSM n,  densify M @= M.
+Proof.
+  intros.
+  unfold densify.
+  unfold "@=".
+  intros.
+  rewrite Mfill_correct; try assumption.
+  reflexivity.
+Qed.
+
+Lemma matrix_eq_commutes :
+  forall (m n: nat) ME M1 M2 (m1: @Mt ME M1 m n) (m2: @Mt ME M2 m n),
+    m1 @= m2 -> m2 @= m1.
+Proof.
+  intros.
+  unfold "@=" in *.
+  intros.
+  rewrite H; auto.
+Qed.
+
+Axiom solveR: forall {n}, SDM n -> SDM n -> SDM n.
+Axiom solveR_correct: forall n: nat, forall M1 M2:SDM n,
+  M1 @* (inversion M2) = solveR M2 M1.
+Lemma multi_assoc: forall n: nat, forall M1 M2 M3 : SDM n,
+      (M1 @* M2) @* M3 @= M1 @* (M2 @* M3).
+Proof.
+  intros.
+  rewrite mult_assoc.
+  reflexivity.
+Qed.
+
+Axiom Cholesky_DC: forall {n}, SDM n -> SDM n.
+Axiom solveR_lower: forall {n}, SDM n -> SDM n -> SDM n.
+Axiom solveR_upper: forall {n}, SDM n -> SDM n -> SDM n.
+Lemma ABv_assoc: forall n:nat, forall A B: SDM n, forall v : Vt n,
+        MVtimes (A @* B) v @=  MVtimes A (MVtimes B v).
+Proof.
+  intros.
+  unfold "&*".
+  rewrite mult_assoc.
+  reflexivity.
+Qed.
+
+Axiom Cholesky_DC_correct: forall n:nat, forall M1 M2 : SDM n, 
+      solveR M1 M2 = solveR_upper (transpose (Cholesky_DC M1)) (solveR_lower (Cholesky_DC M1) M2).
+
+Axiom Densify_correct: forall n:nat, forall M : SDM n, forall S : SSM n,
+        M @= S -> M @= densify S.
+
+
+Lemma Densify_correct_rev: forall n:nat, forall M : SDM n, forall S : SSM n,
+        M @= densify S -> M @= S.
+Proof.
+  intros.
+  unfold densify in H.
+  unfold "@=" in *.
+  intros.
+  rewrite H; auto. 
+  rewrite Mfill_correct; auto.
+Qed.
+
+Axiom dense_sparse_mul: forall {n}, SDM n -> SSM n -> SDM n.
+Axiom dense_sparse_mul_correct: forall {n}, forall A: SDM n, forall B: SSM n, 
+        dense_sparse_mul A B = A @* densify B. 
+Axiom sparse_dense_mul: forall {n}, SSM n -> SDM n -> SDM n.
+Axiom sparse_dense_mul_correct: forall {n}, forall A: SSM n, forall B: SDM n, 
+        sparse_dense_mul A B = densify A @*  B.
+Axiom dense_sparse_mul_to_sparse: forall {n}, SDM n -> SSM n -> SSM n.
+Axiom dense_sparse_mul_to_sparse_correct: forall {n}, forall A: SDM n, forall B: SSM n, 
+        sparsify (dense_sparse_mul A B) = dense_sparse_mul_to_sparse A B. 
+Hint Resolve sparsify_correct densify_correct densify_correct_rev matrix_eq_commutes solveR_correct multi_assoc Densify_correct Densify_correct_rev dense_sparse_mul_correct sparse_dense_mul_correct dense_sparse_mul_to_sparse_correct eq_Mt_refl: matrices.

2d0be23fe6997cbe72c8df11b901d78f273d03a0
Yinzhan Xu <xyzhan@mit.edu>
2018-03-09 22:08:05 -0500

    integrate owl in KalmanAxiom

--- KalmanAxioms.ml
+++ KalmanAxioms.ml
@@ -1,20 +1,25 @@
-type dense_t = float
-type sparse_t = float
+#use "topfind";;
+Topfind.don't_load ["compiler-libs.toplevel"];; 
+#require "owl";;
+open Owl;;
+#require "owl_top";;
+
+let x = Mat.eye 5;;
 
 let cholesky_DC _matrixElem n mat = mat
 
+let dense_fill _matrixElem (nc: int) (nl: int) (fn: int -> int -> float) = Mat.init_2d nc nl fn;; (*  *)
+
+let dense_get _matrixElem (_nc: int) (_nl: int) mat l c = Mat.get mat l c;; (*  *)
+
 let dense_elementwise_op _matrixElem (_nc: int) (_nl: int) (op: float -> float -> float) mat1 mat2 =
-  Obj.magic (op (Obj.magic mat1) (Obj.magic mat2))
+  dense_fill _matrixElem _nc _nl (fun x y -> op (dense_get _matrixElem _nc _nl mat1 x y) (dense_get _matrixElem _nc _nl mat2 x y));;
 
-let dense_fill _matrixElem (nc: int) (nl: int) (fn: int -> int -> float) = Obj.magic (fn 0 0) (*  *)
+let dense_mul _matrixElem (_nc: int) (_nc': int) (_nl: int) mat1 mat2  = Mat.(mat1 * mat2);; (*  *)
 
-let dense_get _matrixElem (_nc: int) (_nl: int) mat l c = (Obj.magic mat) (*  *)
+let dense_sparse_mul _matrixElem n mat1 mat2 = Mat.(mat1 * (Owl_sparse_matrix.D.to_dense mat2));; (*  *)
 
-let dense_mul _matrixElem (_nc: int) (_nc': int) (_nl: int) mat1 mat2  = Obj.magic ((Obj.magic mat1) *. (Obj.magic mat2)) (*  *)
-
-let dense_sparse_mul _matrixElem n mat1 mat2 = Obj.magic ((Obj.magic mat1) *. (Obj.magic mat2)) (*  *)
-
-let dense_sparse_mul_to_sparse _matrixElem n mat1 mat2 = Obj.magic ((Obj.magic mat1) *. (Obj.magic mat2)) (*  *)
+let dense_sparse_mul_to_sparse _matrixElem n mat1 mat2 = Owl_sparse_matrix.D.of_dense (dense_sparse_mul _matrixElem n mat1 mat2);; (*  *)
 
 let float_div x y = x /. y
 
@@ -34,10 +39,10 @@ let float_times x y = x *. y
 
 let float_zero = 0.
 
-let solveR_lower _matrixElem n mat1 mat2 = Obj.magic ((1. /. (Obj.magic mat1)) *. (Obj.magic mat2)) (*  *)
+let solveR_lower _matrixElem n mat1 mat2 = Mat.(Mat.inv mat1 * mat2);; (*  *)
 
-let solveR_upper _matrixElem n mat1 mat2 = Obj.magic ((1. /. (Obj.magic mat1)) *. (Obj.magic mat2)) (*  *)
+let solveR_upper _matrixElem n mat1 mat2 = Mat.(Mat.inv mat1 * mat2);; (*  *)
 
-let sparse_dense_mul _matrixElem n mat1 mat2 = Obj.magic ((Obj.magic mat1) *. (Obj.magic mat2)) (*  *)
+let sparse_dense_mul _matrixElem n mat1 mat2 = Mat.((Owl_sparse_matrix.D.to_dense mat2) * mat1);; (*  *)
 
-let sparse_fill _matrixElem (nc: int) (nl: int) (fn: int -> int -> float) = Obj.magic (fn 0 0) (*  *)
+let sparse_fill _matrixElem (nc: int) (nl: int) (fn: int -> int -> float) = Owl_sparse_matrix.D.of_dense (dense_fill _matrixElem nc nl fn);; (*  *)
